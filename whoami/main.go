d30000863c42730330bda504d8cf7feae766b530
Tero Marttila <tero.marttila@kontena.io>
2016-11-29 00:02:24 +0200

    whoami

---
+++ whoami/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if hostname, err := os.Hostname(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		fmt.Fprintf(w, "%s\n", hostname)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
