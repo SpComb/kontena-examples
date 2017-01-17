929875e16322caac92426278321d47d9c198107a
Tero Marttila <tero.marttila@kontena.io>
2017-01-17 17:02:52 +0200

    net: AddrInfo with DNS names

--- whoami/net.go
+++ whoami/net.go
@@ -6,14 +6,44 @@ import (
   "net/http"
 )
 
+type AddrInfo struct {
+  IP string
+  Port int
+  Name string
+}
+
 type NetworkInfo struct {
-  LocalAddr net.Addr
-  RemoteAddr net.Addr
+  LocalAddr AddrInfo
+  RemoteAddr AddrInfo
+}
+
+func (addrInfo *AddrInfo) resolveDNS() error {
+  if names, err := net.LookupAddr(addrInfo.IP); err != nil {
+    return fmt.Errorf("net.LookupAddr %v: %v", addrInfo.IP, err)
+  } else {
+    for _, name := range names {
+      addrInfo.Name = name
+    }
+  }
+
+  return nil
+}
+
+func (addrInfo *AddrInfo) fromAddr(addr net.Addr) error {
+  switch typeAddr := addr.(type) {
+  case *net.TCPAddr:
+    addrInfo.IP = typeAddr.IP.String()
+    addrInfo.Port = typeAddr.Port
+  default:
+    return fmt.Errorf("Unknown addr type %T: %v", addr, addr)
+  }
+
+  return addrInfo.resolveDNS()
 }
 
 func (networkInfo *NetworkInfo) fromConn(netConn net.Conn) error {
-  networkInfo.LocalAddr = netConn.LocalAddr()
-  networkInfo.RemoteAddr = netConn.RemoteAddr()
+  networkInfo.LocalAddr.fromAddr(netConn.LocalAddr())
+  networkInfo.RemoteAddr.fromAddr(netConn.RemoteAddr())
 
   return nil
 }
@@ -24,13 +54,15 @@ func (networkInfo *NetworkInfo) fromHttp(httpRequest *http.Request) error {
   } else if tcpAddr, err := net.ResolveTCPAddr("tcp", httpRequest.RemoteAddr); err != nil {
     return fmt.Errorf("net.ResolveTCPAddr %v: %v", httpRequest.RemoteAddr, err)
   } else {
-    networkInfo.RemoteAddr = tcpAddr
+    networkInfo.RemoteAddr.fromAddr(tcpAddr)
   }
 
   // this address is just the server listen address :(
   // https://github.com/golang/go/issues/6732#issuecomment-273164492
   if localAddr, ok := httpRequest.Context().Value(http.LocalAddrContextKey).(net.Addr); ok {
-    networkInfo.LocalAddr = localAddr
+    networkInfo.LocalAddr.fromAddr(localAddr)
+  } else {
+    // not available
   }
 
   return nil
